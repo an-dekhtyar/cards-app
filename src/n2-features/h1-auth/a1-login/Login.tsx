0d33234dfda99fabdbdcdd79533f1c2282247c32
Goblester <goblin829@gmail.com>
2021-06-27 21:45:30 +0300

    27.06.2021-init ui example

--- src/n2-features/h1-auth/a1-login/Login.tsx
+++ src/n2-features/h1-auth/a1-login/Login.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import st from './Login.module.css'
+import {PATH} from '../../../n1-main/m1-ui/Routes/Routes';
 
-export const Login = () => (
-    <div className={st.loginPage}>
-        <h1>LOGIN PAGE</h1>
-    </div>
-);
+export const Login: React.FC = () => {
+
+
+    return (
+        <div className={st.loginPage}>
+            <h1>LOGIN PAGE</h1>
+            <label>Email
+                <input/>
+            </label>
+            <label>
+                Password
+                <input/>
+            </label>
+            <NavLink to={PATH.ENTER_NEW_PASS}>Forgot Password</NavLink>
+            <button>Login</button>
+            <p>Don't have an account?</p>
+            <NavLink to={PATH.REGISTRATION}>Sing up</NavLink>
+        </div>
+    )
+}
+
 
