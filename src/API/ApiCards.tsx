494377bdc5b86f83632ac4f54444561912ad1d6c
Goblester <goblin829@gmail.com>
2021-06-27 22:45:16 +0300

    27.06.2021-API-auth/login-done

--- src/API/ApiCards.tsx
+++ src/API/ApiCards.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import axios from "axios";
+import axios from 'axios';
 
 
-let instance=axios.create({
-    baseURL:"http://localhost:7542/2.0/",
-    withCredentials:true,
+let instance = axios.create({
+    baseURL: 'http://localhost:7542/2.0/',
+    withCredentials: true,
     // headers:{
     //     'API-KEY': '0e5dc50f-7e9f-4eda-9157-a63c5026aaad2'
     // }
 })
 
-export const ApiCards={
-    ping(){
+export const ApiCards = {
+    ping() {
         return instance.get(`ping`)
     },
-    addUser(email: string,password: string){
-        return instance.post('/auth/register',{email:email, password:password})
+    addUser(email: string, password: string) {
+        return instance.post('/auth/register', {email: email, password: password})
     },
+    login(email: string, password: string, rememberMe: boolean) {
+        return instance.post<LoginResponseType>('/auth/login', {email, password, rememberMe})
+    }
 
 }
 // {
 //     email: "nya-admin@nya.nya"
 //     password: "1qazxcvBG"
 // }
+
+
+//types
+
+type LoginResponseType = {
+    _id: string
+    email: string
+    name: string
+    avatar?: string
+    publicCardPacksCount: number
+    created: Date
+    updated: Date
+    isAdmin: boolean
+    verified: boolean
+    rememberMe: boolean
+    error?: string
+}
