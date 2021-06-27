480ed4f31e66f33ca9640b5b8eafcd1bae04bf69
login <kakvospitat@gmail.com>
2021-06-27 15:25:11 +0300

    27.06.2021-API-done

---
+++ src/API/ApiCards.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from "axios";
+
+
+let instance=axios.create({
+    baseURL:"http://localhost:7542/2.0/",
+    withCredentials:true,
+    // headers:{
+    //     'API-KEY': '0e5dc50f-7e9f-4eda-9157-a63c5026aaad2'
+    // }
+})
+
+export const ApiCards={
+    ping(){
+        return instance.get(`ping`)
+    },
+    addUser(email: string,password: string){
+        return instance.post('/auth/register',{email:email, password:password})
+    },
+
+}
+// {
+//     email: "nya-admin@nya.nya"
+//     password: "1qazxcvBG"
+// }
