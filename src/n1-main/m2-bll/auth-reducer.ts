684270e0e03b6807aebd25a1fb9ff72091de1ec9
login <kakvospitat@gmail.com>
2021-06-27 19:00:48 +0300

    27.06.2021-AUTH-IT-PATSAN(Sasha Y)-REDUX-done

--- src/n1-main/m2-bll/auth-reducer.ts
+++ src/n1-main/m2-bll/auth-reducer.ts
@@ -1,8 +1,27 @@
 
 
 
+let initialState:Array<string>=[]
 
-
-export const authReducer = (state = {}, action: any): any => { // fix any
-    return state
+export const authReducer = (state = initialState, action: addUserType) => {
+    switch (action.type){
+        case 'addUser':{
+            let newState=[...state]
+            let newItemLoginForm = {email: action.email, password1: action.password, password2: action.password2}
+            // setloginForm([...loginForm, newItemLoginForm])
+                return [...newState, newItemLoginForm]
+        }
+        default :return state
+    }
 };
+
+export type addUserType=ReturnType<typeof addUserAC>
+
+export const addUserAC =(email:string,password:string,password2:string)=>{
+    return {
+        type:'addUser',
+        email:email,
+        password:password,
+        password2:password2
+    }as const
+}
--- src/n2-features/h1-auth/a2-registration/Registration.tsx
+++ src/n2-features/h1-auth/a2-registration/Registration.tsx
@@ -3,34 +3,46 @@ import st from './Registration.module.css'
 import SuperInputText from "../../../n1-main/m1-ui/Common/Input/Input";
 import {Button} from "../../../n1-main/m1-ui/Common/Button/Button";
 import {ApiCards} from "../../../API/ApiCards";
+import {addUserAC, authReducer} from "../../../n1-main/m2-bll/auth-reducer";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../../../n1-main/m2-bll/store";
 
 export type setRegistrationType = {
-    login: '',
+    email: '',
     password1: '',
     password2: ''
 }
 
 export const Registration = () => {
-    let [login, setLogin] = useState('')
+    let [email, setEmail] = useState('')
     let [password1, setPassword1] = useState('')
     let [password2, setPassword2] = useState('')
-    let [loginForm, setloginForm] = useState<any>([])
     let [buttonOn, setbuttonOn] = useState(false)
-    // console.log(loginForm)
+    let[error,setError]=useState(false)
+
+    let dispath = useDispatch()
+    let loginForm = useSelector<AppStoreType, Array<setRegistrationType>>(state => state.auth)
 
     let onclickHandler = () => {
-        // console.log(login, password1, password2)
         setbuttonOn(true);
-        let newItemLoginForm = {login: login, password1: password1, password2: password2}
-        setloginForm([...loginForm, newItemLoginForm])
-        setLogin('')
+        if (password1 === password2) {
+            setError(false)
+            dispath(addUserAC(email, password1, password2))
+        }else{
+            setError(true)
+            setTimeout(()=>{
+                setError(false)
+            },3000)
+        }
+
+        setEmail('')
         setPassword1('')
         setPassword2('')
     }
 
     useEffect(() => {
 
-        ApiCards.addUser( "Shashelephant@gmail.com", "223120Dilan-Bars211")
+        ApiCards.addUser("Shashelephant@gmail.com", "223120Dilan-Bars211")
             .then((res) => {
                 console.log(res.data)
             })
@@ -44,17 +56,22 @@ export const Registration = () => {
     return (
         <div className={st.registrationPage}>
             <h1>REGISTRATION PAGE</h1>
-            <SuperInputText value={login} onChangeText={setLogin}/>
+            <SuperInputText value={email} onChangeText={setEmail}/>
             <SuperInputText value={password1} onChangeText={setPassword1}/>
             <SuperInputText value={password2} onChangeText={setPassword2}/>
 
+            {error===true && <div className={st.PasswordError}>Check YOUR PASSWORD</div>}
+
             <Button children={'Send'} onClick={() => onclickHandler()}/>
+
             {buttonOn &&
             loginForm.map((m: setRegistrationType) => {
                 return (
                     <div className={st.buttonOn}>
-                        <div>Your Login={m.login}</div>
+                        <div>Your Login={m.email}</div>
                         <div>Your Password={m.password1}</div>
+
+
                     </div>
                 )
             })
