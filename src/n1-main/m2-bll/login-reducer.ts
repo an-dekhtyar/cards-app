435a970fef122ebf28289e1857e5b871d8f0fe6a
Goblester <goblin829@gmail.com>
2021-06-27 22:26:18 +0300

    27.06.2021-login state

--- src/n1-main/m2-bll/login-reducer.ts
+++ src/n1-main/m2-bll/login-reducer.ts
@@ -1,10 +1,30 @@
 
 
+const initialState = {
+       isLoading: false,
+       _id: '1',
+       email: 'example@gmail.com',
+       name: 'user1',
+       rememberMe: false,
+       avatar: null as string | null,
+       publicCardPacksCount: null as number | null,
+       created: '01.01.2021',
+       updated: '01.06.2021',
+       isAdmin: false,
+       verified: false,
+       error: null as string | null
+}
 
+export type LoginReducerStateType = typeof  initialState;
 
-export const loginReducer = (state = {}, action: any): any => { // fix any
+export const loginReducer = (state = initialState, action: any): LoginReducerStateType => { // fix any
 
+       switch (action.type){
+
+       }
 
        return state;
 
 };
+
+type LoginActionsType = {}
--- src/n2-features/h1-auth/a1-login/Login.tsx
+++ src/n2-features/h1-auth/a1-login/Login.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {NavLink} from 'react-router-dom';
 import st from './Login.module.css'
 import {PATH} from '../../../n1-main/m1-ui/Routes/Routes';
+import {useSelector} from 'react-redux';
+import {AppStoreType} from '../../../n1-main/m2-bll/store';
 
 export const Login: React.FC = () => {
+    const [email, setEmail] = useState(useSelector<AppStoreType, string>(state => state.login.email))
+    const [password, setPassword] = useState<string>('');
+    const [rememberMe, setRememberMe] = useState<boolean>(useSelector<AppStoreType, boolean>(state => state.login.rememberMe))
 
+    const onEmailInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value);
+    }
+
+    const onPasswordInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.currentTarget.value);
+    }
+
+    const onRememberMeInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setRememberMe(e.currentTarget.checked);
+    }
 
     return (
         <div className={st.loginPage}>
             <h1>LOGIN PAGE</h1>
-            <label>Email
-                <input type={'email'}/>
+            <label>Email <input type={'email'}
+                                value={email}
+                                onChange={onEmailInputChange}/>
             </label>
             <label>
-                Password
-                <input type={'password'}/>
+                Password <input type={'password'}
+                                value={password}
+                                onChange={onPasswordInputChange}/>
             </label>
-            <label>Remember me
-                <input type={'checkbox'}/>
+            <label>Remember me <input type={'checkbox'}
+                                      checked={rememberMe}
+                                      onChange={onRememberMeInputChange}/>
             </label>
             <NavLink to={PATH.ENTER_NEW_PASS}>Forgot Password</NavLink>
             <button>Login</button>
