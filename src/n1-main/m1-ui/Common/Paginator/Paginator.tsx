6e10eb98401265a009546f3824264d532135b7c5
Goblester <goblin829@gmail.com>
2021-07-09 16:00:36 +0300

    09.07.2021-paginator rework-done

--- src/n1-main/m1-ui/Common/Paginator/Paginator.tsx
+++ src/n1-main/m1-ui/Common/Paginator/Paginator.tsx
@@ -1,5 +1,5 @@
 import s from './Paginator.module.css';
-import React, {useState} from 'react';
+import React from 'react';
 
 
 type PaginatorPropsType = {
@@ -16,38 +16,53 @@ export const Paginator = (props: PaginatorPropsType) => {
         portionSize
     } = props;
 
-    const [currentPortion, setCurrentPortion]= useState<number>(1);
-
     const totalPortionCount = Math.ceil(itemCount / portionSize);
-    const leftBorderItem = (currentPortion - 1) * portionSize + 1;
-    const rightBorderItem = currentPortion * portionSize;
 
     let items = [];
-    for (let i = leftBorderItem; i <= rightBorderItem; i++) {
-        items.push(i);
+    let i = currentItem - 4;
+    if (totalPortionCount - currentItem < 5) {
+        i = i - (5 - totalPortionCount + currentItem);
+    }
+    while (items.length < 10 && i <= totalPortionCount) {
+        if (items.length === 0 && currentItem > 5 && totalPortionCount > portionSize) {
+            items.push(1)
+        } else if (items.length === 9 && totalPortionCount - currentItem > 5 && totalPortionCount > portionSize) {
+            items.push(totalPortionCount);
+        } else if (i > 0) {
+            items.push(i)
+        }
+        i++;
     }
 
-    const onPrevButtonClick = ()=>{
-        setCurrentPortion(currentPortion-1)
+
+    const onPrevButtonClick = () => {
+        props.onPageChanged(currentItem - 1)
     }
-    const onNextButtonClick = ()=>{
-        setCurrentPortion(currentPortion+1);
+    const onNextButtonClick = () => {
+        props.onPageChanged(currentItem + 1)
     }
 
     const pagesComponent = items.map(p => {
         const onClick = () => {
+            debugger;
             props.onPageChanged(p);
         }
         return (
-            <span key={p} className={`${p === currentItem ? s.currentItem : ''} ${s.item}`} onClick={onClick}>{p}</span>
+            <>
+                {totalPortionCount > 10 && totalPortionCount - currentItem > 5 && p === totalPortionCount && '...'}
+                <span key={p} className={`${p === currentItem ? s.currentItem : ''} ${s.item}`}
+                      onClick={onClick}>{p}</span>
+                {currentItem > 5 && p === 1 && totalPortionCount > 10 && '...'}
+            </>
         )
     });
 
     return (
         <div>
-            {currentPortion!==1&&<button className={s.btn} onClick={onPrevButtonClick}>Prev</button>}
+            {currentItem !== 1 && <button className={s.btn} onClick={onPrevButtonClick}>Prev</button>}
             {pagesComponent}
-            {currentPortion!==totalPortionCount&&<button className={s.btn} onClick={onNextButtonClick}>Next</button>}
+            {currentItem !== totalPortionCount &&
+            <button className={s.btn} onClick={onNextButtonClick}>Next</button>}
         </div>
     );
 }
