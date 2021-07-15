90bf6747599d200cf6db1207ebb1c02c5aa1917d
Goblester <goblin829@gmail.com>
2021-07-15 12:18:47 +0300

    14.07.2021-learn-done

--- src/n2-features/h2-cards/a3-process-of-learning/Learn.tsx
+++ src/n2-features/h2-cards/a3-process-of-learning/Learn.tsx
@@ -1,12 +1,74 @@
 import {Button} from '../../../n1-main/m1-ui/Common/Button/Button';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import SuperRadio from '../../../n1-main/m1-ui/Common/Radio/SuperRadio';
+import {useHistory} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {CardType, UpgradeCardGradeThunk} from '../../../n1-main/m2-bll/cards-reducer';
+import {AppStoreType} from '../../../n1-main/m2-bll/store';
+
+
+const getCard = (cards: Array<CardType>, curCard_id: string) => {
+    const newCards = cards.filter(cards => cards._id !== curCard_id);
+    const gradeCount = newCards.reduce((count, card) => {
+        return count + (6 - card.grade) ** 2;
+    }, 0);
+    let rndNumber = gradeCount * Math.random();
+
+    let i = 0;
+
+    while (rndNumber > 0) {
+        rndNumber = rndNumber - (6 - cards[i].grade) ** 2;
+        i++;
+    }
+    return cards[i - 1];
+}
 
 
 export const Learn = () => {
+    //state
+    const dispatch = useDispatch();
     const [showAnswer, setShowAnswer] = useState<boolean>(false);
-    const [value, setValue] = useState<string>('Did not know')
-    const options = ['Did not know', 'Forgot', 'A lot of thoughts', 'Confused', 'Knew the answer']
+    const [value, setValue] = useState<string>('Did not know');
+    const grades = ['Did not know', 'Forgot', 'A lot of thoughts', 'Confused', 'Knew the answer'];
+    const cards = useSelector<AppStoreType, Array<CardType>>(state => state.cards.cards);
+    const [curCard, setCurCard] = useState<CardType>({
+        answer: '',
+        answerImg: '',
+        answerVideo: '',
+        cardsPack_id: '',
+        comments: '',
+        created: '',
+        grade: 3,
+        more_id: '',
+        question: 'no cards',
+        questionImg: '',
+        questionVideo: '',
+        rating: 3,
+        shots: 1,
+        type: '',
+        updated: '',
+        user_id: '',
+        __v: 1,
+        _id: '',
+    });
+
+    useEffect(() => {
+        if (cards.length === 0) {
+            return;
+        }
+        setCurCard(getCard(cards, curCard._id));
+    }, []);
+    useEffect(() => {
+        setValue(grades[curCard.grade - 1])
+    }, [curCard])
+
+    const history = useHistory();
+
+    //functions
+
+    const onCancelButtonClick = () => {
+        history.goBack();
+    }
 
     const onShowAnswerButtonClick = () => {
         setShowAnswer(true);
@@ -14,22 +76,31 @@ export const Learn = () => {
     const onOptionChange = (option: string) => {
         setValue(option);
     }
+    const onNextButtonClick = () => {
+        const newGrade = grades.indexOf(value) + 1;
+        dispatch(UpgradeCardGradeThunk(curCard._id, newGrade));
+        setShowAnswer(false);
+        setCurCard(getCard(cards, curCard._id));
+
+    }
+
     return (
         <div>
             <h2>Learn 'PackName'</h2>
-            <p>Question: bla bla</p>
+            <p>Question: {curCard.question}</p>
             {
-                showAnswer && <p>Answer: bla bla</p>}
+                showAnswer && <p>Answer: {curCard.answer}</p>}
             {showAnswer && <div>
                 <SuperRadio value={value}
-                            options={options}
+                            options={grades}
                             onChangeOption={onOptionChange}/>
             </div>}
-            <Button>Cancel</Button>
+            <Button onClick={onCancelButtonClick}>Cancel</Button>
             {!showAnswer ?
-                <Button onClick={onShowAnswerButtonClick}>Show Answer</Button>
+                <Button disabled={cards.length === 0}
+                        onClick={onShowAnswerButtonClick}>Show Answer</Button>
                 :
-                <Button onClick={onShowAnswerButtonClick}>Next</Button>}
+                <Button onClick={onNextButtonClick}>Next</Button>}
         </div>
     )
 }
